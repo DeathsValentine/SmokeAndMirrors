c259b30490b5a74a13db83d5d82858c11094f706
Matteo <61305877+HarryParryTestes@users.noreply.github.com>
2021-04-14 23:32:49 -0700

    fmod + music and foosteps test

---
+++ Assets/Scripts/TriggerAudio.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAudio : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string Event;
+    public bool PlayOnAwake;
+
+    public void PlayOneShot()
+    {
+        FMODUnity.RuntimeManager.PlayOneShotAttached(Event, gameObject);
+    }
+    // Start is called before the first frame update
+    private void Start()
+    {
+        if (PlayOnAwake)
+            PlayOneShot();
+    }
+    
+}
