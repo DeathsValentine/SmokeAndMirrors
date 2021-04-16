b8933c710fbde45a08996d909c71854696ccab1c
Matteo <61305877+HarryParryTestes@users.noreply.github.com>
2021-04-15 23:40:20 -0700

    fmod stuff, banks, footsteps, ice attack

---
+++ Assets/Scripts/TriggerAnimationAudio.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAnimationAudio : MonoBehaviour
+{
+    
+    private string Event;
+    public bool PlayOnAwake;
+
+    public void PlaySound(string Event)
+    {
+        if (Event != null)
+        {
+            FMODUnity.RuntimeManager.PlayOneShot(Event);
+        }
+        else
+        {
+            Debug.LogError("EventSound is null");
+        }
+
+    }
+}
