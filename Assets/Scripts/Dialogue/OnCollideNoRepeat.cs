ca232fcb51c5a7627037beafa36a43b079238767
Valentine <shadow_hero@rocketmail.com>
2021-04-23 19:18:57 -0700

    Text

---
+++ Assets/Scripts/Dialogue/OnCollideNoRepeat.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollideNoRepeat : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("I TOUCHED THE BOX");
+        StartCoroutine(Dialogue.Instance.StartTalking());
+        gameObject.SetActive(false);
+    }
+}
