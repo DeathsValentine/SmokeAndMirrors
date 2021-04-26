107190ddee15ea25d5f0ddf83854a7aaa4febc46
Valentine <shadow_hero@rocketmail.com>
2021-04-26 13:29:07 -0700

    Revert "Put the grass back"
    This reverts commit 76c4e7812637783f66b9d45375964871b387e268.

--- Assets/Scripts/Dialogue/DialogueData.cs
+++ Assets/Scripts/Dialogue/DialogueData.cs
@@ -2,16 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace RPG.Dialogue {
-    [CreateAssetMenu(fileName = "New DialogData", menuName = "Dialog Data")]
-    public class DialogData : ScriptableObject
-    {
-        [SerializeField]
-        private List<string> sentences = new List<string>();
+[CreateAssetMenu(fileName = "New DialogData", menuName = "Dialog Data")]
+public class DialogData : ScriptableObject
+{
+    [SerializeField]
+    private List<string> sentences = new List<string>();
 
-        public List<string> GetSentences()
-        {
-            return sentences;
-        }
+    public List<string> GetSentences()
+    {
+        return sentences;
     }
-};
+}
--- Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using RPG.Dialogue;
 
 public class DialogueTrigger : MonoBehaviour
 {
