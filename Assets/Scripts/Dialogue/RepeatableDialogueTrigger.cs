d1e2604097277aad7476d356619f1684ebbaefbc
Valentine <shadow_hero@rocketmail.com>
2021-04-29 17:18:09 -0700

    Repeatable Dialogue is On Click

--- Assets/Scripts/Dialogue/RepeatableDialogueTrigger.cs
+++ Assets/Scripts/Dialogue/RepeatableDialogueTrigger.cs
@@ -6,10 +6,27 @@ public class RepeatableDialogueTrigger : MonoBehaviour
 {
     public DialogManager dialogManager;
     public string dialogData;
+    public bool isClose = false;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
+        {
+            isClose = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            isClose = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (isClose && Input.GetKeyDown(KeyCode.Mouse0))
         {
             dialogManager.dialogData = dialogData;
             dialogManager.BeginDialog();
