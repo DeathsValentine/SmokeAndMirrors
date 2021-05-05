e8c851d4c4df88efda76e1e379876234357ca9d7
Ikenna N Eke <ieke@mail.sfsu.edu>
2021-05-05 15:11:06 -0700

    Local Networking

---
+++ Assets/Scripts/JoinServer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class JoinServer : NetworkManager
+{
+    // Start is called before the first frame update
+    public override void Awake()
+    {
+        if (!NetworkServer.active)
+        {
+            StartHost();
+            Debug.Log("Creating Host Server");
+        }
+        else if(NetworkServer.active)
+        {
+            networkAddress = "localhost";
+            StartClient();
+            networkAddress = "localhost";
+        }
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
