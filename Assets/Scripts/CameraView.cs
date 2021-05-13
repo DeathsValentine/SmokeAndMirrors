ee194e40dd0b8bd0a4e2d325654cae78c3c7e6f1
Ikenna N Eke <ieke@mail.sfsu.edu>
2021-05-12 22:04:04 -0700

    players can join Servers

--- Assets/Scripts/CameraView.cs
+++ Assets/Scripts/CameraView.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Mirror;
 public class CameraView : MonoBehaviour
 {
     GameObject player;
@@ -9,13 +9,21 @@ public class CameraView : MonoBehaviour
 
     void Start()
     {
-        player = GameObject.FindWithTag("Player");
-        camPos = GameObject.FindWithTag("MainCamera").transform.position;
+        if (!NetworkServer.active)
+        {
+            player = GameObject.FindWithTag("Player");
+            camPos = GameObject.FindWithTag("MainCamera").transform.position;
+        }
     }
+
     // Update is called once per frame
     void Update()
     {
-        
-        this.transform.position = GameObject.FindWithTag("Player").transform.position + camPos;
+        if (player != null)
+        {
+            this.transform.position = GameObject.FindWithTag("Player").transform.position + camPos;
+        }
     }
+
+   
 }
---
+++ Assets/Scripts/Networking/joinServer.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class joinServer : NetworkManager
+{
+    public struct createCharacMessage : NetworkMessage
+    {
+        public string character;
+    }
+
+    public bool isServer = false;
+
+    // Start is called before the first frame update
+
+
+    public override void Awake()
+    {
+        base.Awake();
+        if (isServer)
+        { 
+            StartHost();
+        }
+        else
+        {
+            StartClient();
+        }
+
+    }
+
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        NetworkServer.RegisterHandler<createCharacMessage>(OnCreateCharac);
+    }
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+
+        createCharacMessage mssg = new createCharacMessage {
+            character = PlayerPrefs.GetString("Character")
+        };
+
+        conn.Send(mssg);
+    }
+
+    public void OnCreateCharac(NetworkConnection conn,createCharacMessage msg)
+    {
+        
+        Transform startPos = GetStartPosition();
+        GameObject playerPrefab = spawnPrefabs[0];
+        
+        if (msg.character == "Merlyn")
+        {
+            playerPrefab = spawnPrefabs[1];
+        }
+        
+        Debug.Log(playerPrefab);
+            GameObject player = startPos != null
+            ? Instantiate(playerPrefab, startPos.position, Quaternion.identity)
+            : Instantiate(playerPrefab);
+        Debug.Log(player);
+        // instantiating a "Player" prefab gives it the name "Player(clone)"
+        // => appending the connectionId is WAY more useful for debugging!
+        player.name = $"{playerPrefab.name} [connId={conn.connectionId}]";
+        NetworkServer.AddPlayerForConnection(conn, player);
+     
+        
+    }
+    
+
+   
+}
